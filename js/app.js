267058d65d7d7f2a062ba2dad941d964090e50cb
jahidulis <jahidulislam.exm@gmail.com>
2022-02-17 11:45:50 +0600

    total expenses finished

--- js/app.js
+++ js/app.js
@@ -4,4 +4,23 @@ document.getElementById('calculate-button').addEventListener('click',function() 
     const incomeInput = document.getElementById('income-value');
     const incomeAmmount = incomeInput.value;
     console.log(incomeAmmount);
+    // Total Balance Left
+    const totalBalance = document.getElementById('total-balance');
+    totalBalance.innerText = incomeAmmount;
+    // total expenses
+    // food cost
+    const foodCostInput = document.getElementById('food-cost');
+    const totalFoodCost = foodCostInput.value;
+    console.log(totalFoodCost);
+    // rent cost
+    const rentCostInput = document.getElementById('rent-cost');
+    const totalRentCost = rentCostInput.value;
+    console.log(totalRentCost);
+    // clothes cost
+    const coltheCostInput = document.getElementById('clothes-cost');
+    const totalClothesCost = coltheCostInput.value;
+    console.log(totalClothesCost);
+    // total Expenses
+    const totalExpenses = document.getElementById('total-expenses');
+    totalExpenses.innerText = parseFloat(totalFoodCost) + parseFloat(totalRentCost) + parseFloat(totalClothesCost);
 })
